da0c2a492378c5aedb562e2b20179588db370fe3
atharvp <atharvep@gmail.com>
2025-02-16 03:08:50 +0530

    Adding Exit Feature

--- Calculator/main.py
+++ Calculator/main.py
@@ -53,9 +53,14 @@ def calculator():
             print(f"{num2} {operation_symbol} {num1} = {answer}")
         else:
             print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        end=input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation or type 'exit' to end calculation : ")
+    
 
-        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+        if end == "y":
             num1 = answer
+        elif end.lower()== "exit":
+            break   
         else:
             should_continue = False
             calculator()
