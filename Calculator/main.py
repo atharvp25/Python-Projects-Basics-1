3149c7440fc18a74313f9cdca2ddfe46defe6f82
atharvp <atharvep@gmail.com>
2025-02-16 02:59:41 +0530

    Fix Space in line 57

--- Calculator/main.py
+++ Calculator/main.py
@@ -54,8 +54,7 @@ def calculator():
         else:
             print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        if input(f"Type 'y' to continue calculating with {answer},\
-             or type 'n' to start a new calculation: ") == "y":
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
             num1 = answer
         else:
             should_continue = False
